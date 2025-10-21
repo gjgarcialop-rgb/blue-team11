307c4b57fcdb5002196d0a36f76bc06f94d775e9
Giovanni Garcia Lopez <gjgarcialop@uncg.edu>
2025-10-21 13:45:03 -0400

    Add Spring Boot backend-api project structure (without database credentials)

---
+++ backend-api/src/main/java/BlueTeam11/ChillCrib/ChillCribApplication.java
@@ -0,0 +1,13 @@
+package BlueTeam11.ChillCrib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillCribApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChillCribApplication.class, args);
+	}
+
+}
---
+++ backend-api/src/test/java/BlueTeam11/ChillCrib/ChillCribApplicationTests.java
@@ -0,0 +1,13 @@
+package BlueTeam11.ChillCrib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChillCribApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
